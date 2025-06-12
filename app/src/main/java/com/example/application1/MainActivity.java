e9b1e29385cb85b2394b57824d2cddb59523e0de
yoyo2050123 <yoyo2050123@gmail.com>
2025-06-12 10:02:28 +0800

    2025 0611 下午

--- app/src/main/java/com/example/application1/MainActivity.java
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.application1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckedTextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+     CheckedTextView mCheckedTextView01, mCheckedTextView02;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_button);
+        setContentView(R.layout.activity_chat_gpt_test);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
+//      m  });
+//        mCheckedTextView01 = (CheckedTextView) findViewById(R.id.checkedTextView01);
+//        mCheckedTextView01.setChecked(true);
+//        mCheckedTextView01.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                mCheckedTextView01.toggle();
+//            }
+//        });
+//        mCheckedTextView02 = (CheckedTextView) findViewById(R.id.checkedTextView02);
+//
+//        mCheckedTextView02.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mCheckedTextView02.toggle();
+//            }
 //        });
     }
 }
